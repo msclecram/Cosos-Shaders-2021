70baafc63dd9a37168d4e733df87ed441fc55508
Zyumane <64087119+Zyumane@users.noreply.github.com>
2021-03-18 20:59:40 -0600

    up to now
    los cambios de Shaders

---
+++ JorgeZ-210121/Unity/Color in Shadow/Assets/180321/PostScriptsPor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostScriptsPor : MonoBehaviour
+{
+    public Material mat;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, mat);
+    }
+}
