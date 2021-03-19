14cd508f2c5eb00de1a50a85339a0e9c7bd6a784
OsoCoco <ccordoba@cocoschool.mx>
2021-03-18 21:00:54 -0600

    Ejercicio Clase

---
+++ Ivan-210121/Shaders1/Assets/180321/Scripts/ScreenEffects.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenEffects : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Material mat;
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, mat);
+    }
+}
