ed8d920d143224e6e30e328e8f5e916114e2eb2f
AngelOswaldo <aornelas@cocoschool.mx>
2021-05-04 20:06:35 -0500

    Avances-040521

---
+++ AngelOswaldo/ProyectoFinal_Shaders_Unity/Assets/Scripts/CameraPositions.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPositions : MonoBehaviour
+{
+    public List<Transform> cameraPositions;
+    public Transform myCamera;
+    public int actualIndex = 0;
+
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            GoBack();
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            GoFront();
+        }
+
+    }
+
+    private void GoBack()
+    {
+        actualIndex -= 1;
+        if(actualIndex<=0)
+        {
+            actualIndex = cameraPositions.Count - 1;
+        }
+        myCamera.position = cameraPositions[actualIndex].position;
+        myCamera.rotation = cameraPositions[actualIndex].rotation;
+    }
+
+    private void GoFront()
+    {
+        actualIndex += 1;
+        if (actualIndex >= cameraPositions.Count)
+        {
+            actualIndex = 0;
+        }
+        myCamera.position = cameraPositions[actualIndex].position;
+        myCamera.rotation = cameraPositions[actualIndex].rotation;
+    }
+
+}
